097c64ea6b3fdd0a272e27bb882641819f4247d5
heinhtetaung-node <heinhtetaung.sglife@gmail.com>
2018-08-12 18:00:50 +0630

    change to server url

--- src/axioConfig.js
+++ src/axioConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axioApi = axios.create({  
-        baseURL: 'http://localhost:5000/api/',
-        //baseURL: 'http://mern-stack-course.herokuapp.com/api/',  // let our base uri is server uri
+        //baseURL: 'http://localhost:5000/api/',
+        baseURL: 'http://mern-stack-course.herokuapp.com/api/',  // let our base uri is server uri
         //baseURL: 'https://jsonplaceholder.typicode.com/',
         timeout: 10000,
         withCredentials: true,
